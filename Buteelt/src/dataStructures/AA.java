7a4ab946df502ae369c1ccd911b2acbcb06f7bb2
Bayar1223 <158148986+Bayar1223@users.noreply.github.com>
2024-01-30 18:07:06 +0800

    Update AA.java

--- Buteelt/src/dataStructures/AA.java
+++ Buteelt/src/dataStructures/AA.java
@@ -1,11 +1,11 @@
 package dataStructures;
 
 public class AA {
-
 	public static void main(String []args) 
 	{
 		System.out.println("Hello World");
 		System.out.println("Test");
+		System.out.println("Test2 Commit changes to eclipse");
 	}
 	
 }
